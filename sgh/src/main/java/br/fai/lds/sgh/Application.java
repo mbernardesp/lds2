6390a2a3d925307c5a9c37f6efcfa4aa5787c573
Marcelo Bernardes Pinto <marcelobernardespinto@gmail.com>
2019-07-31 00:58:11 -0300

    - Configuração do pom.xml - Configuração da porta e contexto no arquivo application.properties

---
+++ sgh/src/main/java/br/fai/lds/sgh/Application.java
@@ -0,0 +1,21 @@
+package br.fai.lds.sgh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Marcelo
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);        
+    }
+    
+}
+
+
+
