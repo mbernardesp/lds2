334d8410d6e727d51957a028acdd3618ed932bef
Marcelo Bernardes Pinto <marcelobernardespinto@gmail.com>
2019-10-29 21:35:19 -0300

    - Manipulação de imagens utilizando Base64

--- sgh-client/src/main/java/br/fai/lds/sgh/client/controller/dashboard/DashboardController.java
+++ sgh-client/src/main/java/br/fai/lds/sgh/client/controller/dashboard/DashboardController.java
@@ -5,10 +5,20 @@
  */
 package br.fai.lds.sgh.client.controller.dashboard;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -16,13 +26,51 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class DashboardController {
+    
+    private static final String NOT_FOUND_IMG = ""; 
+    private static final String UPLOADED_FOLDER = "c:/Users/Marcelo/Pictures/"; 
 
     @GetMapping("dashboard/list")
     public String dashboard(Model model) throws IOException {
 
         model.addAttribute("msg", "SGH Dashboard");
+        
+        File file = new File(UPLOADED_FOLDER + "img.png");
+        
+        if(file.exists()){
+            
+            byte[] fileContent = FileUtils.readFileToByteArray(file);
+            
+            String encodeString = Base64.getEncoder().encodeToString(fileContent);
+            
+            model.addAttribute("base64Image", encodeString);
+        }else{
+            
+            model.addAttribute("base64Image", NOT_FOUND_IMG);
+        }
+        
 
         return "dashboard";
 
+    }
+    
+    @PostMapping("dashboard/upload")
+    public String dashboardUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        
+        if(!file.isEmpty()){
+            
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + "img.png");
+                Files.write(path,bytes);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            return "redirect:/dashboard/list";
+        }
+        
+        return "redirect:/error";
     }
 }
--- sgh-client/src/main/webapp/WEB-INF/jsp/dashboard.jsp
+++ sgh-client/src/main/webapp/WEB-INF/jsp/dashboard.jsp
@@ -6,6 +6,19 @@
 <div class="container">
     <h1>Dashboard</h1>
     <span>${msg}</span>
+    
+    <br/>
+    <br/>
+    
+    <form method="POST" action="${pageContext.request.contextPath}/dashboard/upload" enctype="multipart/form-data">
+      <input type="file" name="file"/> 
+      <br/>  
+      <br/>  
+      <input type="submit" value="Save">
+    </form>
+      
+    <img src="data:image/png;base64,${base64Image}" width="240" height="300"/>  
+      
 </div>
 
 <jsp:include page="fragments/footer.jsp" />
